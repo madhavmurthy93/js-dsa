f049d15264bd2147b25f4ffc40ce3a84b5fe63a4
Madhav Murthy <madhavmurthy93@gmail.com>
2024-06-10 10:54:19 +0530

    Memoize fibonacci, dynamic programming

--- recursion/fib.js
+++ recursion/fib.js
@@ -1,8 +1,15 @@
-function fib(num) {
+function fib(num, memo=[]) {
+    if (memo[num] != undefined) {
+        return memo[num];
+    }
     if (num === 1 || num === 2) return 1;
 
-    return fib(num - 1) + fib(num - 2);
+    let res = fib(num - 1, memo) + fib(num - 2, memo);
+    memo[num] = res;
+    return res;
 }
 
 console.log(fib(4));
 console.log(fib(10));
+console.log(fib(40));
+console.log(fib(100));
